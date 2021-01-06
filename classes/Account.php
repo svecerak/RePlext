a8613908b78b96d683650c624d4234a62e0d7b45
Sasa <svecerak@uwo.ca>
2021-01-06 12:42:27 -0500

    Spacing issues

--- classes/Account.php
+++ classes/Account.php
@@ -1,204 +1,204 @@
 <?php
 
-	class Account {
+class Account {
 
-		// Instance variables
+	// Instance variables
 
-        private $con;
-		private $errorArray;
+	private $con;
+	private $errorArray;
 
 
-		/**
-		 * Class constructor
-		 */
-		public function __construct($con) { // pass in $con vairable from register.php
-            $this->con = $con; // assigning the con var from register.php to the private instance var in this class
-			$this->errorArray = array();
-		}
- 
+	/**
+	 * Class constructor
+	 */
+	public function __construct($con) { // pass in $con vairable from register.php
+		$this->con = $con; // assigning the con var from register.php to the private instance var in this class
+		$this->errorArray = array();
+	}
 
-		/**
-		 * Login function
-		 * TODO: Poor security, change to bcrypt
-		 */
-        public function login($username, $password) {
-            $pw = md5($password);
-			$p_username = '';
-			$p_pass = '';			
 
-			// SQL Query
+	/**
+	 * Login function
+	 * TODO: Poor security, change to bcrypt
+	 */
+	public function login($username, $password) {
+		$pw = md5($password);
+		$p_username = '';
+		$p_pass = '';			
 
-			$sql =  "SELECT * FROM users ";
-			$sql .= "WHERE username = ? ";
-			$sql .= "AND password = ?";
+		// SQL Query
 
-			// Prepare statement
+		$sql =  "SELECT * FROM users ";
+		$sql .= "WHERE username = ? ";
+		$sql .= "AND password = ?";
 
-			if ($stmt = $this->con->prepare($sql)) {
-				$stmt->bind_param("ss", $p_username, $p_pass);
-				$p_username = $username;
-				$p_pass = $pw;
+		// Prepare statement
 
-				if ($stmt->execute()) {
-					$result = $stmt->get_result();
+		if ($stmt = $this->con->prepare($sql)) {
+			$stmt->bind_param("ss", $p_username, $p_pass);
+			$p_username = $username;
+			$p_pass = $pw;
 
-					if ($result->num_rows == 1) {
-						return true;
-					} else {
-						$this->errorArray[] = Constants::$loginFailed;
-						return false;
-					}
+			if ($stmt->execute()) {
+				$result = $stmt->get_result();
+
+				if ($result->num_rows == 1) {
+					return true;
+				} else {
+					$this->errorArray[] = Constants::$loginFailed;
+					return false;
 				}
-			} 
-
-			// Close statement and connection
-
-			$stmt->close();
-			$this->con->close();
-         }    
-			
-
-
-		/**
-		 * Registration function
-		 */
-		public function register($un, $em, $em2, $pw, $pw2) {
-			$this->validateUsername($un);
-			$this->validateEmails($em, $em2);
-			$this->validatePasswords($pw, $pw2);
-
-			if(empty($this->errorArray)) {
-                return $this->insertUserDetails($un, $em, $pw);
-			} else {
-				return false;
 			}
+		} 
 
-		}
+		// Close statement and connection
 
-
-		/**
-		 * Check for errors
-		 */
-		public function getError($error) {
-			if(!in_array($error, $this->errorArray)) {
-				$error = "";
-			}
-			return "<span class='errorMessage'>$error</span>";
-        }
+		$stmt->close();
+		$this->con->close();
+		}    
 		
+
+
+	/**
+	 * Registration function
+	 */
+	public function register($un, $em, $em2, $pw, $pw2) {
+		$this->validateUsername($un);
+		$this->validateEmails($em, $em2);
+		$this->validatePasswords($pw, $pw2);
+
+		if(empty($this->errorArray)) {
+			return $this->insertUserDetails($un, $em, $pw);
+		} else {
+			return false;
+		}
+
+	}
+
+
+	/**
+	 * Check for errors
+	 */
+	public function getError($error) {
+		if(!in_array($error, $this->errorArray)) {
+			$error = "";
+		}
+		return "<span class='errorMessage'>$error</span>";
+	}
+	
+	
+	/**
+	 * Insert information into database
+	 * TODO: Poor security hashing function, change to bcrypt
+	 */
+	private function insertUserDetails($un, $em, $pw) {
+		$encryptedPw = md5($pw); 
+		$profile_image = "assets/images/profile-pics/head_emerald.png";
+		$date = date("Y-m-d");
+		$id = '';
+
+		// SQL Query
+
+		$sql = "INSERT INTO users ";
+		$sql .= "VALUES (?, ?, ?, ?, ?, ?)";
+
+		// Prepare statement
+
+		$stmt = $this->con->prepare($sql);
+		$stmt->bind_param("ssssss", $id, $un, $em, $encryptedPw, $profile_image, $date);
+		$stmt->execute();
+
+		return $stmt;
+	}
 		
-		/**
-		 * Insert information into database
-		 * TODO: Poor security hashing function, change to bcrypt
-		 */
-        private function insertUserDetails($un, $em, $pw) {
-            $encryptedPw = md5($pw); 
-            $profile_image = "assets/images/profile-pics/head_emerald.png";
-			$date = date("Y-m-d");
-			$id = '';
 
-			// SQL Query
-
-			$sql = "INSERT INTO users ";
-			$sql .= "VALUES (?, ?, ?, ?, ?, ?)";
-
-			// Prepare statement
-
-			$stmt = $this->con->prepare($sql);
-			$stmt->bind_param("ssssss", $id, $un, $em, $encryptedPw, $profile_image, $date);
-			$stmt->execute();
-
-			return $stmt;
-		}
-			
-
-		/**
-		 * Validate username and save errors to array
-		 * 
-		 */
-		private function validateUsername($username) {
-			if(strlen($username) > 25 || strlen($username) < 5) {
-				$this->errorArray[] = Constants::$usernameCharacters;
-				return;
-			}
-
-			// SQL query
-
-			$sql  = "SELECT username ";
-			$sql .= "FROM users ";
-			$sql .= "WHERE username=?";
-
-			// Prepared statement
-
-			$stmt = $this->con->prepare($sql);
-			$stmt->bind_param("s", $username);
-			$stmt->execute();
-
-			// Check whether username has already been taken
-
-			$result = $stmt->get_result();
-
-			if($result->num_rows != 0) {
-				$this->errorArray[] = Constants::$usernameTaken;
-				return;
-			}
-		}
-
-
-		/**
-		 * Validate emails, ensure they match
-		 */
-		private function validateEmails($em, $em2) {
-
-			if($em != $em2) {
-				$this->errorArray[] = Constants::$emailsDoNotMatch;
-				return;
-			}
-
-			else if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
-				$this->errorArray[] = Constants::$emailInvalid;
-				return;
-			}
-
-			// SQL
-
-			$sql  = "SELECT email ";
-			$sql .= "FROM users ";
-			$sql .= "WHERE email=?";
-
-			// Prepared statement
-
-			$stmt = $this->con->prepare($sql);
-			$stmt->bind_param("s", $em);
-			$stmt->execute();
-
-			// Check whether email has already been taken
-
-			$result = $stmt->get_result();
-			
-			if($result->num_rows != 0) {
-				$this->errorArray[] = Constants::$emailTaken;
-			}
-
+	/**
+	 * Validate username and save errors to array
+	 * 
+	 */
+	private function validateUsername($username) {
+		if(strlen($username) > 25 || strlen($username) < 5) {
+			$this->errorArray[] = Constants::$usernameCharacters;
 			return;
 		}
 
+		// SQL query
 
-		/**
-		 * Validate passwords, ensure matching entries
-		 */
-		private function validatePasswords($pw, $pw2) {
-			
-			if($pw != $pw2) 
-				$this->errorArray[] = Constants::$passwordsDoNoMatch;
-			
-			else if(preg_match('/[^A-Za-z0-9]/', $pw)) 
-				$this->errorArray[] = Constants::$passwordNotAlphanumeric;
-			
-			else if(strlen($pw) > 30 || strlen($pw) < 5) 
-				$this->errorArray[] = Constants::$passwordCharacters;
+		$sql  = "SELECT username ";
+		$sql .= "FROM users ";
+		$sql .= "WHERE username=?";
 
+		// Prepared statement
+
+		$stmt = $this->con->prepare($sql);
+		$stmt->bind_param("s", $username);
+		$stmt->execute();
+
+		// Check whether username has already been taken
+
+		$result = $stmt->get_result();
+
+		if($result->num_rows != 0) {
+			$this->errorArray[] = Constants::$usernameTaken;
 			return;
 		}
 	}
+
+
+	/**
+	 * Validate emails, ensure they match
+	 */
+	private function validateEmails($em, $em2) {
+
+		if($em != $em2) {
+			$this->errorArray[] = Constants::$emailsDoNotMatch;
+			return;
+		}
+
+		else if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+			$this->errorArray[] = Constants::$emailInvalid;
+			return;
+		}
+
+		// SQL
+
+		$sql  = "SELECT email ";
+		$sql .= "FROM users ";
+		$sql .= "WHERE email=?";
+
+		// Prepared statement
+
+		$stmt = $this->con->prepare($sql);
+		$stmt->bind_param("s", $em);
+		$stmt->execute();
+
+		// Check whether email has already been taken
+
+		$result = $stmt->get_result();
+		
+		if($result->num_rows != 0) {
+			$this->errorArray[] = Constants::$emailTaken;
+		}
+
+		return;
+	}
+
+
+	/**
+	 * Validate passwords, ensure matching entries
+	 */
+	private function validatePasswords($pw, $pw2) {
+		
+		if($pw != $pw2) 
+			$this->errorArray[] = Constants::$passwordsDoNoMatch;
+		
+		else if(preg_match('/[^A-Za-z0-9]/', $pw)) 
+			$this->errorArray[] = Constants::$passwordNotAlphanumeric;
+		
+		else if(strlen($pw) > 30 || strlen($pw) < 5) 
+			$this->errorArray[] = Constants::$passwordCharacters;
+
+		return;
+	}
+}
 ?>
