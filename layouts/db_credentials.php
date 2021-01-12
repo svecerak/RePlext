c303595b6f7dd8c2252acf099d0eb45df181edb0
Sasa <svecerak@uwo.ca>
2021-01-12 02:48:27 -0500

    remote connection values

--- layouts/db_credentials.php
+++ layouts/db_credentials.php
@@ -1,13 +1,13 @@
 <?php
 
 // Local
-define('DB_SERVER', "localhost");
-define('DB_USERNAME', "root");
-define('DB_PASS', "");
-define('DB_NAME', "replext");
+// define('DB_SERVER', "localhost");
+// define('DB_USERNAME', "root");
+// define('DB_PASS', "");
+// define('DB_NAME', "replext");
 
 // Remote connection
-// define('DB_SERVER', "freedb.tech");
-// define('DB_USERNAME', "freedbtech_svecerak");
-// define('DB_PASS', "mydemopw34");
-// define('DB_NAME', "freedbtech_replext");
+define('DB_SERVER', "freedb.tech");
+define('DB_USERNAME', "freedbtech_svecerak");
+define('DB_PASS', "mydemopw34");
+define('DB_NAME', "freedbtech_replext");
