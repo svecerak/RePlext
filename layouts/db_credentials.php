228a426bfc0ba8fa9e340b09b4f7aa1b9ff7fb55
Sasa <svecerak@uwo.ca>
2021-01-12 00:56:11 -0500

    credentials for remote server

--- layouts/db_credentials.php
+++ layouts/db_credentials.php
@@ -1,6 +1,13 @@
 <?php
 
-define('DB_SERVER', "localhost");
-define('DB_USERNAME', "root");
-define('DB_PASS', "");
+// Local
+// define('DB_SERVER', "localhost");
+// define('DB_USERNAME', "root");
+// define('DB_PASS', "");
+// define('DB_NAME', "replext");
+
+// Remote connection
+define('DB_SERVER', "db4free.net");
+define('DB_USERNAME', "sasavec");
+define('DB_PASS', "1cool134");
 define('DB_NAME', "replext");
